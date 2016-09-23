dd395248a5d4d70657e9b4e1ea187a28ccab88c4
isabelbicalho <isabel_bicalho@yahoo.com.br>
2016-09-23 20:17:28 -0300

    Teste

--- Grace/Grace/Controllers/MessagesController.cs
+++ Grace/Grace/Controllers/MessagesController.cs
@@ -45,6 +45,7 @@ namespace Grace
             {
                 // Implement user deletion here
                 // If we handle user deletion, return a real message
+                return message.CreateReply("Deletando dados do usuário");
             }
             else if (message.Type == ActivityTypes.ConversationUpdate)
             {
@@ -52,22 +53,25 @@ namespace Grace
                 // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
                 // Not available in all channels
                 //return message.CreateReply("Olá, sou Grace!");
+                return message.CreateReply("Atualizando conversação");
             }
             else if (message.Type == ActivityTypes.ContactRelationUpdate)
             {
                 // Handle add/remove from contact lists
                 // Activity.From + Activity.Action represent what happened
-                return message.CreateReply("Olá, sou Grace!");
+                return message.CreateReply("Atualizando relação.");
             }
             else if (message.Type == ActivityTypes.Typing)
             {
                 // Handle knowing tha the user is typing
+                return message.CreateReply("Você está digitando?");
             }
             else if (message.Type == ActivityTypes.Ping)
             {
+                return message.CreateReply("Ping!");
             }
 
-            return null;
+            return message.CreateReply("Olá, sou Grace!");
         }
     }
 }
